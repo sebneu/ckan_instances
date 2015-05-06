6868f18e1968a4bdd28e03457536f1e5f990ecab
Sebastian Neumaier <seb.neumaier@gmail.com>
2015-05-06 19:12:29 +0200

    ckan instances csv

--- ckan_instances.py
+++ ckan_instances.py
@@ -2,8 +2,15 @@ from __future__ import print_function
 import urllib2
 from urlparse import urlparse
 import json
+import csv
 
 DATAPORTALS = 'http://dataportals.org/api/data.json'
+
+
+def get_id_from_url(url):
+    parsed_uri = urlparse(url)
+    id = '{uri.netloc}'.format(uri=parsed_uri)
+    return id.replace('.', '_')
 
 
 def lookup_dataportals(dataportals):
@@ -22,27 +29,47 @@ def lookup_dataportals(dataportals):
                 try:
                     # check if url exists
                     urllib2.urlopen(url)
-                    parsed_uri = urlparse(url)
-                    id = '{uri.netloc}'.format(uri=parsed_uri)
-                    id = id.replace('.', '_')
 
-                    api = 'not found'
+                    # get id
+                    id = get_id_from_url(url)
+
+                    api = None
                     if api_url:
                         try:
                             urllib2.urlopen(api_url)
                             api = api_url
                         except:
-                            pass
+                            print(id+': api error')
+                    ckan_portals[id] = {'url': url, 'api': api}
                 except urllib2.HTTPError, e:
                     print(e.code)
                 except urllib2.URLError, e:
                     print(e.args)
+    return ckan_portals
 
-                ckan_portals[id] = {'url': url, 'api': api}
+
+def read_csv(file_obj):
+    csvf = csv.reader(file_obj)
+    ckan_portals = {}
+
+    # my portals are stored in 2 columns, url and api
+    for row in csvf:
+        url = row[0]
+        api = row[1]
+
+        id = get_id_from_url(url)
+        if api == 'not found':
+            api = None
+
+        ckan_portals[id] = {'url': url, 'api': api}
     return ckan_portals
 
 if __name__ == '__main__':
+    with open('instances.csv') as f:
+        csvlist = read_csv(f)
+
     p = lookup_dataportals(DATAPORTALS)
 
-    for l in p:
-        print(l)
+    for key in p:
+        if key not in csvlist:
+            print(key + ' ' + str(p[key]['api']))
